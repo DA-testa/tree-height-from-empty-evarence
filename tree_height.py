fd741c7d549c022a28d67740572331d23b5882fd
evarence <124592587+evarence@users.noreply.github.com>
2023-03-10 21:26:28 +0200

    test5

--- tree_height.py
+++ tree_height.py
@@ -5,39 +5,55 @@ import threading
 
 
 def compute_height(n, parents):
-    lev = [-1] * n
-    
-    def max_height(x): 
-        if x == (-1):
-            return -1  
-        
-        if lev[x] == (-1):
-            lev[x] = 1 + max_height(parents[x])
-        return lev[x]    
+    # Write this function
+    n = int(n)
+    level = [0] * n
+    count = [0] * n
+    lev = []
+    c=0
+    for i in range(0, n):
+        c = 1
+        while parents[i] >= 0:
+            i = parents[i]
+            c=c+1
+        lev.append(c)
 
-    for x in range(n):
-        max_height(x)
-    return max(lev) +1
+    
+    max_height = 0
+    for i in lev:
+        if lev[i]>max_height:
+            max_height = lev[i]
+ 
+    #print(max_height)
+    return max_height
 
 
 def main():
     mode = input()
     if "I" in mode:
-        n = int(input())
-        text = input()
+        count = int(input())
+        text = input() 
+  
         text = text.split()
-        parents = (list(map(int, text)))
-        print(compute_height(n, parents))
+        text = map(int, text)
+        text = list(text)
 
-    elif "F" in mode:
+        print(compute_height(count, text))
+
+    elif "I" in mode:
         name = input()
         if name.find("a")!=-1:
             return
         with open("./test/" + name, mode="r") as fails:
             count = fails.readline()
             text = fails.readline()
-            parents = (list(map(int, text.split())))
-            print(compute_height(count, text))
+  
+        text = text.split()
+        text = map(int, text)
+        text = list(text)
+        print(compute_height(count, text))
        
-main()
-    
+
+sys.setrecursionlimit(10**7)  # max depth of recursion
+threading.stack_size(2**27)   # new thread will get stack of such size
+threading.Thread(target=main).start()
